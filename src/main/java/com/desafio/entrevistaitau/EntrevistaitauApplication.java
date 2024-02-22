4e660f5efab9dd2ac849bcd6448c432fa3535689
Diego da Silva Santana <diegosantanasantos@yahoo.com.br>
2024-02-22 17:43:39 -0300

    primeiro commit

---
+++ src/main/java/com/desafio/entrevistaitau/EntrevistaitauApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.entrevistaitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistaitauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistaitauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/entrevistaitau/EntrevistaitauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.entrevistaitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaitauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
